d4ced6a7c3f77aba15ae09375dc24f08790b2584
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-13 13:28:07 +0900

    5.htmlを表示するようにしました

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html',)
 
 
+@app.route('/5')
+def page5():
+    return render_template('5.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
