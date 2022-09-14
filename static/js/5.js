5d19b77f05a2de18d1a938f48a8fad3d1756df37
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-14 14:10:00 +0900

    微修正

--- static/js/5.js
+++ static/js/5.js
@@ -1,11 +1,16 @@
 $(function () {
+    // （1）Web Storageの実装確認
+    if (typeof sessionStorage === 'undefined') {
+        window.alert("このブラウザはWeb Storage機能が実装されていません");
+    } else {
+        window.alert("実装されてます");
+    }
 
     // class'showUp'をdiv要素に追加
     $('.present_box').addClass('showUp');
 
     //4.000秒後に実行
     setTimeout(function () {
-        // $('.present').attr('src', 'static/img/seed_of_himawari.png');
         $('.himawari').addClass('UP');
         $('.present').addClass('DOWN');
     }, 3000);
