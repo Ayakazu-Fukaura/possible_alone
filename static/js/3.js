88c8094d9824e70fcfda5b1aa693ccf32f3b0c15
“yutomura” <yuto0724yuki@icloud.com>
2022-09-15 13:29:25 +0900

    Javaでとりあえず、変更

--- static/js/3.js
+++ static/js/3.js
@@ -1,5 +1,9 @@
 $(function(){
-    tasks = ['トイレ', 'かおあらい', 'ごはん', 'かばんじゅんび', 'はみがき', 'トイレ']
-    $("h1").text(tasks[5])
+    viewStorage()
+    var tasks = storage.getItem("tasks");
+        $("h1").text(tasks[0])
 
+        $('.btn').on('click', function () {
+            $("h1").getItem("tasks[1]")
+          });
 })
