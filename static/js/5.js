311fe4d0f6cdcf22c7e051f01aa345a89bb62ea9
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-15 12:30:55 +0900

    ページ5でwebStorageのデータを読み込めるようにしました

--- static/js/5.js
+++ static/js/5.js
@@ -1,11 +1,4 @@
 $(function () {
-    // （1）Web Storageの実装確認
-    if (typeof sessionStorage === 'undefined') {
-        window.alert("このブラウザはWeb Storage機能が実装されていません");
-    } else {
-        window.alert("実装されてます");
-    }
-
     // class'showUp'をdiv要素に追加
     $('.present_box').addClass('showUp');
 
@@ -15,5 +8,6 @@ $(function () {
         $('.present').addClass('DOWN');
     }, 3000);
 
+    viewStorage();
 
 });
