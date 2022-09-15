a59c1fc20d22f44b1c0f011cb64e8bccd07d891f
“yutomura” <yuto0724yuki@icloud.com>
2022-09-15 20:57:58 +0900

    4_toilet.jsを削除

--- static/js/4.js
+++ static/js/4.js
@@ -2,3 +2,19 @@ var crrent_task = JSON.parse(storage.getItem("crrent_task"));
 crrent_task = crrent_task + 1
 storage.setItem("crrent_task", JSON.stringify(crrent_task));
 
+
+
+
+
+$(function(){
+    var toilet_task = JSON.parse(storage.getItem("toilet_task"));
+    toilet_task = toilet_task + 1
+
+    
+storage.setItem("toilet_task", JSON.stringify(toilet_task));
+    viewStorage()
+    var tasks = JSON.parse(storage.getItem("tasks"));
+    var toilet_task = JSON.parse(storage.getItem("toilet_task"));
+        $(".says").text(tasks[toilet_task] + "してくれてありがとう!")
+        console.log(toilet_task)
+})
