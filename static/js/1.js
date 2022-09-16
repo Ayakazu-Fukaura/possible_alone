366ce9935dba280845d783f5e98b82ab4072893b
“yutomura” <yuto0724yuki@icloud.com>
2022-09-16 12:20:09 +0900

    1.jsから2.jsに変更

--- static/js/1.js
+++ static/js/1.js
@@ -9,15 +9,5 @@ function showClock1() {
  }
  setInterval('showClock1()',1000);
 
-$(function () {
-    setlocalStorage();
-var crrent_task = 0
-storage.setItem("crrent_task", JSON.stringify(crrent_task));
-
-var toilet_task = -1
-storage.setItem("toilet_task", JSON.stringify(toilet_task));
-console.log(toilet_task)
-});
-
 
 
---
+++ static/js/2.js
@@ -0,0 +1,9 @@
+$(function () {
+    setlocalStorage();
+var crrent_task = 0
+storage.setItem("crrent_task", JSON.stringify(crrent_task));
+
+var toilet_task = -1
+storage.setItem("toilet_task", JSON.stringify(toilet_task));
+console.log(toilet_task)
+});
