76d3761f2be3c2f73de2e103741b14470d58d631
“yutomura” <yuto0724yuki@icloud.com>
2022-09-20 12:40:05 +0900

    toilet_taskをcrrent_taskで統一

--- static/js/2.js
+++ static/js/2.js
@@ -3,7 +3,4 @@ $(function () {
 var crrent_task = 0
 storage.setItem("crrent_task", JSON.stringify(crrent_task));
 
-var toilet_task = -1
-storage.setItem("toilet_task", JSON.stringify(toilet_task));
-console.log(toilet_task)
 });
--- static/js/4.js
+++ static/js/4.js
@@ -7,16 +7,11 @@ storage.setItem("crrent_task", JSON.stringify(crrent_task));
 
 
 $(function(){
-    var toilet_task = JSON.parse(storage.getItem("toilet_task"));
-    toilet_task = toilet_task + 1
-
-    
-storage.setItem("toilet_task", JSON.stringify(toilet_task));
-    viewStorage()
     var tasks = JSON.parse(storage.getItem("tasks"));
-    var toilet_task = JSON.parse(storage.getItem("toilet_task"));
-        $(".says").text(tasks[toilet_task] + "してくれてありがとう!")
-        console.log(toilet_task)
+
+    viewStorage();
+    $(".says").text(tasks[crrent_task - 1] + "してくれてありがとう!")
+
 
         
 })
