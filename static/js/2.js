ccd25d27428280dd0b0dba558fe4d00a223a5eed
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-20 16:12:11 +0900

    リストが長くなってもスクロールで対応します

--- static/js/2.js
+++ static/js/2.js
@@ -1,6 +1,17 @@
 $(function () {
+    //初期タスクセット
     setlocalStorage();
-var crrent_task = 0
-storage.setItem("crrent_task", JSON.stringify(crrent_task));
 
+    //crrent_taskを初期化
+    var crrent_task = 0
+    storage.setItem("crrent_task", JSON.stringify(crrent_task));
+
+    //タスクリストを表示
+    var tasks = [];
+    tasks = JSON.parse(storage.getItem("tasks"));
+    $('.content').append('<div>' + tasks[0][0] + '</div>');
+    for (let i = 1; i < tasks.length; i++) {
+        $('.content').append('<img src="/static/img/pink_yajirusi.png" alt="写真"><div>' + tasks[i][0] + '</div>');
+
+    }
 });
