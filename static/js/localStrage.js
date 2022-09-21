c8467645e9a4bdecd29783d67d67bda4ef72461f
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-21 11:41:36 +0900

    衝突回避

--- static/js/localStrage.js
+++ static/js/localStrage.js
@@ -3,49 +3,6 @@
 // <script src="/static/js/localStrage.js"></script>
 // を追記してください。
 
-<<<<<<< HEAD
-=======
-// （1）Web Storageの実装確認
-// if (typeof sessionStorage === 'undefined') {
-//     window.alert("このブラウザはWeb Storage機能が実装されていません");
-// } else {
-//     console.log("このブラウザはWeb Storage機能を実装しています");
-//     var storage = localStorage;
-
-
-
-//     // （2）sessionStorageへの格納
-//     function setlocalStorage() {
-//         console.log("初期taskをセット")
-//         var tasks = [];
-//         tasks = [["トイレ", "toilet.png"], ["顔洗い", "kao.png"], ["ごはん", "gohan.png"], ["かばん準備", "kaban.png"], ["はみがき", "hamigaki.png"], ["トイレ", "toilet.png"]];
-//         // 値の入力
-//         storage.setItem("tasks", JSON.stringify(tasks));
-//         viewStorage();
-//     }
-
-
-
-
-
-//     // （3）sessionStorageからのデータの取得と表示
-//     function viewStorage() {
-//         var tasks = JSON.parse(storage.getItem("tasks"));
-//         console.log("リストを表示します：" + tasks);
-//     }
-
-
-
-//     // （5）sessionStorageからすべて削除
-//     function removeallStorage() {
-//         storage.clear();
-//         viewStorage();
-//     }
-// }
-
-
-
->>>>>>> 5319ea85ac8c3f5dc8330f0f3bebf0dbd0345a08
 
 // （1）Web Storageの実装確認
 if (typeof sessionStorage === 'undefined') {
