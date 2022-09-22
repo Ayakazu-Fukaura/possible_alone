02283b1abe5e14d061b5797593e8cab931c0fcca
“yutomura” <yuto0724yuki@icloud.com>
2022-09-22 11:52:23 +0900

    0.jsで写真をインプットさせた

--- static/js/0.js
+++ static/js/0.js
@@ -1,18 +1,22 @@
 $(function() {
+    var local_src = "";
     var sexial = "男";
     var name = "";
     $('.Button-style').on('click', function() {
         sexial = $('input:radio[name="c1"]:checked').val();
-        viewStorage("sexial");
-
         name = $(".text").val();
-        console.log(name)
-
         window.location.href = "/00";
+
         storage.setItem("sexial", JSON.stringify(sexial));
         storage.setItem("name", JSON.stringify(name));//これが本体
       })
 
+    $('#Character_btn button').on('click', function(){
+        local_src = this.children[0].children[0].getAttribute("src")
+        
+        storage.setItem("local_src", JSON.stringify(local_src));
+        viewStorage("local_src");
+    })
 
 
   });
--- static/js/00.js
+++ static/js/00.js
@@ -1,8 +1,11 @@
 $(function(){
     var sexial = "男";
     var name = "";
-    sexial = JSON.parse(storage.getItem("sexial"))
+    var local_src = "";
+    sexial = JSON.parse(storage.getItem("sexial"));
     name = JSON.parse(storage.getItem("name"));
+    local_src = JSON.parse(storage.getItem("local_src"))
     $(".input_text").val(name)
     $(".kun").val(sexial)
+    $('.img').attr('src', local_src);
 });
