dd6941fbfaf8002265ab5d40f3f480672779da2a
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-22 23:42:10 +0900

    文字サイズを可変にしました

--- static/js/3.js
+++ static/js/3.js
@@ -12,6 +12,23 @@ $(function () {
         console.log(tasks[crrent_task][1]);
     }
 
+    var height = 0, body_height = 0;
+    body_height = document.getElementById("body").offsetHeight;
+    height = document.getElementById("task_text").offsetHeight;
+    var check = document.getElementById("task_text").style.fontSize;
+    console.log("bodyの高さ:" + body_height);
+    console.log("調整前高さ：" + height);
+    console.log("check:" + check)
+
+    for (let size = 70; height > body_height - 170 && size > 1; size--) {
+        document.getElementById("task_text").style.fontSize = size + "px";
+        height = document.getElementById("task_text").offsetHeight;
+
+    }
+    // height = document.getElementById("task_text").offsetHeight;
+
+    console.log("調整後：" + height);
+
 })
 
 
