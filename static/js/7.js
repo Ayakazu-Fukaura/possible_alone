7dd9d736c5a03d964fe1c4d13615d716d31f3b80
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-22 17:05:40 +0900

    性別と名前を反映させるようにしました

--- static/js/7.js
+++ static/js/7.js
@@ -1,59 +1,72 @@
-$(function() {
+$(function () {
     var lock = 0;
-    function omikuji() {
-     const cont = Math.ceil(Math.random()*4);
-     if(cont == 1){
-       $(".omikuji_name").html("楽しい１日が ぜったいきます！");
-       $(".omikuji_text").html("末吉と思いきや...? 大吉でした〜！！");
-       $(".omikuji_img").attr("src","/static/img/daikiti.png");
-       $('.omikuji_img').attr('src', '/static/img/suekiti.png')
-    setTimeout(function () {
-        $('.omikuji_img').attr('src', '/static/img/daikiti.png')
-    }, 1000);
-       
-   }
-       if(cont == 2){
-       $(".omikuji_name").html("きぶんがいい １日になりそう！");
-       $(".omikuji_text").html("中吉がでました！やったね！");
-       $(".omikuji_img").attr("src","/static/img/tyukiti.png");
-    
-   }
-       if(cont == 3){
-       $(".omikuji_name").html("学校ついたら いいことあるかも！");
-       $(".omikuji_text").html("小吉がでました！ラッキー！");
-       $(".omikuji_img").attr("src","/static/img/syokiti.png");
-   }
-       if(cont == 4){
-       $(".omikuji_name").html("コツコツやれば だいじょうぶ！");
-       $(".omikuji_text").html("末吉がでました！がんばろう！");
-       $(".omikuji_img").attr("src","/static/img/suekiti.png");
-   }
+
+    var name = JSON.parse(storage.getItem("name"));
+    var sexial = JSON.parse(storage.getItem("sexial"));
+    console.log(sexial);
+    if (!name || name == "") {
+        $(".name").html("みんなげんきで、<br>いってらっしゃい♪");
+
+    } else {
+
+        $(".name").html(name + sexial + "、<br>いってらっしゃい♪");
+
     }
-   
-   
-       $(".omiguji_btn").on("click",function () {
-           if(lock == 0) {
-                $(".omiguji_btn").addClass("gray");
-                omikuji();
-                lock = 1;
-           }
-       });
 
+    function omikuji() {
+        const cont = Math.ceil(Math.random() * 4);
+        if (cont == 1) {
+            $(".omikuji_name").html("楽しい１日が ぜったいきます！");
+            $(".omikuji_text").html("末吉と思いきや...? 大吉でした〜！！");
+            $(".omikuji_img").attr("src", "/static/img/daikiti.png");
+            $('.omikuji_img').attr('src', '/static/img/suekiti.png')
+            setTimeout(function () {
+                $('.omikuji_img').attr('src', '/static/img/daikiti.png')
+            }, 1000);
 
-       function load_effect() {
-        var element = document.getElementsByClassName('omikuji_img');
-        if(!element) return; // 要素がない場合は終了
-        
-        for(var i = 0; i < element.length; i++) { 
-          element[i].classList.add('is-show');
         }
-      }
-      setTimeout(load_effect, 1000); // 600ミリ秒経過後に実行
+        if (cont == 2) {
+            $(".omikuji_name").html("きぶんがいい １日になりそう！");
+            $(".omikuji_text").html("中吉がでました！やったね！");
+            $(".omikuji_img").attr("src", "/static/img/tyukiti.png");
 
-      $('#page-top').fadeIn(10000);
+        }
+        if (cont == 3) {
+            $(".omikuji_name").html("学校ついたら いいことあるかも！");
+            $(".omikuji_text").html("小吉がでました！ラッキー！");
+            $(".omikuji_img").attr("src", "/static/img/syokiti.png");
+        }
+        if (cont == 4) {
+            $(".omikuji_name").html("コツコツやれば だいじょうぶ！");
+            $(".omikuji_text").html("末吉がでました！がんばろう！");
+            $(".omikuji_img").attr("src", "/static/img/suekiti.png");
+        }
+    }
 
 
-       });
+    $(".omiguji_btn").on("click", function () {
+        if (lock == 0) {
+            $(".omiguji_btn").addClass("gray");
+            omikuji();
+            lock = 1;
+        }
+    });
+
+
+    function load_effect() {
+        var element = document.getElementsByClassName('omikuji_img');
+        if (!element) return; // 要素がない場合は終了
+
+        for (var i = 0; i < element.length; i++) {
+            element[i].classList.add('is-show');
+        }
+    }
+    setTimeout(load_effect, 1000); // 600ミリ秒経過後に実行
+
+    $('#page-top').fadeIn(10000);
+
+
+});
 
 
 
