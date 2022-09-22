c95405e2954c12e93b77758a43814523802e1bdd
“Ayakazu-Fukaura” <muzumi.hamis.11515@gmail.com>
2022-09-22 16:50:24 +0900

    名前表示設定

--- static/js/1.js
+++ static/js/1.js
@@ -2,12 +2,30 @@
 function showClock1() {
     var nowTime = new Date();
     var nowHour = nowTime.getHours();
-    var nowMin  = nowTime.getMinutes();
-    var nowSec  = nowTime.getSeconds();
-    var msg = "いまのじかんは、" + nowHour + "時" + nowMin + "分" + nowSec + "秒" +"です。";
+    var nowMin = nowTime.getMinutes();
+    var nowSec = nowTime.getSeconds();
+    var msg = "いまのじかんは、" + nowHour + "時" + nowMin + "分" + nowSec + "秒" + "です。";
     document.getElementById("RealtimeClockArea").innerHTML = msg;
- }
- setInterval('showClock1()',1000);
+}
+setInterval('showClock1()', 1000);
+
+$(function () {
+    var name = JSON.parse(storage.getItem("name"));
+    var sexial = JSON.parse(storage.getItem("sexial"));
+    console.log(sexial);
+    if (!name || name == "") {
+        $(".title").text("みんな～ おはよう！");
+
+    } else {
+        if (sexial == "ちゃん") {
+            $(".title").text(name + "ちゃん おはよう！");
+        } else {
+            $(".title").text(name + "くん おはよう！");
+        }
+    }
+
+
+});
 
 
 
