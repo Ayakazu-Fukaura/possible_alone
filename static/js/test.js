1b835cf549b17ae340f96acf128c7116f8985e21
komatsuyukiko <ume-yupi.2-2.3-2@yahoo.ne.jp>
2022-09-24 14:26:59 +0900

    漢字をひらがなに変更

--- static/js/test.js
+++ static/js/test.js
@@ -9,13 +9,13 @@ $(function () {
     tasks = JSON.parse(storage.getItem("tasks"));
 
     if (!all_task) {
-        all_task = [["トイレ", "toilet.png"], ["ごはん", "gohan.png"], ["はみがき", "hamigaki.png"], ["かばん準備", "kaban.png"], ["顔洗い", "kao.png"],];
+        all_task = [["トイレ", "toilet.png"], ["しょくじ", "gohan.png"], ["はみがき", "hamigaki.png"], ["かばんようい", "kaban.png"], ["かおあらい", "kao.png"],];
         setlocalStorage("all_task", all_task);
         console.log("all_taskを初期設定しました");
     }
     viewStorage("all_task");
     if (!tasks) {
-        tasks = [["トイレ", "toilet.png"], ["顔洗い", "kao.png"], ["しょくじ", "gohan.png"], ["かばん準備", "kaban.png"], ["はみがき", "hamigaki.png"], ["トイレ", "toilet.png"]];
+        tasks = [["トイレ", "toilet.png"], ["かおあらい", "kao.png"], ["しょくじ", "gohan.png"], ["かばんようい", "kaban.png"], ["はみがき", "hamigaki.png"], ["トイレ", "toilet.png"]];
         setlocalStorage("tasks", tasks);
         console.log("tasksを初期設定しました");
     }
